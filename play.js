153854f2be951906e3d559224a404c10a23d25b6
Aaron Au <aaron.au@me.com>
2022-04-13 23:56:03 +0000

    Refactor code to listen for keyboard input by writing a setup function.

--- play.js
+++ play.js
@@ -1,5 +1,27 @@
 //import module
 const { connect } = require('./client');
 
+//handleUserInput function. this is the callback for stdin.on() on line 20
+const handleUserInput = function(key) {
+  //'\u0003 maps to crtl+c to input. This kills the program
+  if (key === '\u0003') process.exit();
+};
+
 console.log("Connecting ...");
 connect();
+
+//function to listen for keybod input
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf-8');
+  stdin.resume();
+
+  //implement event listener
+  stdin.on('data', handleUserInput);
+
+  return stdin;
+};
+
+//function call
+setupInput();
