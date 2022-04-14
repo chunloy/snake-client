abaac1e2ae2bde65e5772ba9d5f656a47119df3a
Aaron Au <aaron.au@me.com>
2022-04-14 01:33:24 +0000

    Initial commit. Constants for use in client.js & input.js

---
+++ constants.js
@@ -0,0 +1,9 @@
+//Define constants
+const IP = '165.227.47.243';
+const PORT = 50541;
+const NAME = "Name: ARN";
+const EXIT = '\u0003';
+
+
+//export module
+module.exports = { IP, PORT, NAME, EXIT };
